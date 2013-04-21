553df257c458def7aebd2b86bd84954e28dec6c4
ajlopez <webmaster@ajlopez.com>
2013-04-21 07:52:20 -0300

    Simple Model Vacuum Agent first tests

---
+++ test/simplemodelvacuumagent.js
@@ -0,0 +1,19 @@
+
+var agents = require('..').agents;
+var assert = require('assert');
+
+assert.ok(agents);
+
+var agent = agents.createSimpleModelVacuumAgent();
+var actions = agents.SimpleVacuumAction;
+var locations = agents.SimpleVacuumLocation;
+var status = agents.VacuumStatus;
+
+var result = agent.program({ status: status.Dirty, location: locations.A });
+assert.equal(result, actions.Suck);
+
+var result = agent.program({ status: status.Clean, location: locations.A });
+assert.equal(result, actions.Right);
+
+var result = agent.program({ status: status.Clean, location: locations.B });
+assert.equal(result, actions.NoOp);
