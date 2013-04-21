cb9720b52f7912d9992346d72537a654fe538d81
ajlopez <webmaster@ajlopez.com>
2013-04-21 07:23:07 -0300

    Random Vacuum Agent, first tests

---
+++ test/randomvacuumagent.js
@@ -0,0 +1,23 @@
+
+var agents = require('..').agents;
+var assert = require('assert');
+
+assert.ok(agents);
+
+var agent = agents.createRandomVacuumAgent();
+var actions = agents.VacuumAction;
+
+var result = agent.program({});
+
+assert.ok(result);
+assert.ok(result === actions.NoOp || result === actions.Suck || result === actions.Left || result == actions.Right);
+
+var results = [];
+
+for (var k = 0; k < 100; k++)
+    results.push(agent.program({}));
+    
+assert.ok(results.indexOf(actions.NoOp) >= 0);
+assert.ok(results.indexOf(actions.Suck) >= 0);
+assert.ok(results.indexOf(actions.Left) >= 0);
+assert.ok(results.indexOf(actions.Right) >= 0);
